4100c7f9a0a99089d124888028a64c0de3c8d83f
hguzman <hguzman@IATGULSDES1634.ADBancat.hn>
2024-09-26 00:30:37 -0600

    Adding Swagger configuration to the service.

--- src/main/java/com/cuscatlan/payments/application/dto/PaymentRequestDto.java
+++ src/main/java/com/cuscatlan/payments/application/dto/PaymentRequestDto.java
@@ -1,5 +1,6 @@
 package com.cuscatlan.payments.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +12,15 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Details of the payment request")
 public class PaymentRequestDto {
+
+    @Schema(description = "ID of the associated order", example = "12345")
     private Long orderId;
+
+    @Schema(description = "Amount of the payment", example = "99.99", required = true)
     private BigDecimal amount;
+
+    @Schema(description = "Currency of the payment", example = "USD", allowableValues = {"USD", "EUR", "GBP"})
     private String currency;
 }
