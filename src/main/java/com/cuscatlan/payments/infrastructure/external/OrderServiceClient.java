a1c99b852e7c0f2dddb6814a4d169b01a5ce626a
davecalix83 <davecalix@gmail.com>
2024-09-27 01:05:44 -0600

    Adding configuration for Eureka Server and routing management through Api Gateway.

--- src/main/java/com/cuscatlan/payments/infrastructure/external/OrderServiceClient.java
+++ src/main/java/com/cuscatlan/payments/infrastructure/external/OrderServiceClient.java
@@ -3,7 +3,6 @@ package com.cuscatlan.payments.infrastructure.external;
 import com.cuscatlan.payments.application.dto.OrderDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "order-service",
-        url = "http://localhost:8080/api/v1/orders")
+        url = "http://localhost:8082/api/v1/orders")
 public interface OrderServiceClient {
 
 
